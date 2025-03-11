36679cc14d8af01745717ef8f5ec4965377a162d
mohit1229 <kumarmohiiit123@gmail.com>
2025-03-11 17:15:58 +0530

    UI refined

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import './App.css';
-
+import TaskSvg from './assets/task-square-svgrepo-com.svg'
 function InputArea() {
   const [text, setText] = useState('');
   const [height, setHeight] = useState(40); // Initial height
@@ -11,6 +11,7 @@ function InputArea() {
   };
 
   return (
+    <div className='flex'>
     <div className="bg-stone-800 w-full rounded-lg p-4">
       <textarea
         placeholder="Add tasks"
@@ -19,34 +20,67 @@ function InputArea() {
         value={text}
         onInput={handleInput}
       ></textarea>
+            <button  className="bg-fuchsia-800 text-white rounded-lg p-3 mt-4 ">Add Task</button>
+            </div>
+
     </div>
   );
 }
+function TaskList() {
+  return (
+    <div className=" rounded-lg w-full mt-4 p-4 text-center">Task List
+    <div className='grid grid-cols-4 gap-4 mt-4'>
+    <Card />
+    <Card />
+    <Card />
+    <Card />
+    <Card />
+    <Card />
+    
+    </div>
+    </div>
+  );
+}
+function Card(){
+  return (
+    <div className="bg-stone-900 w-full rounded-lg border-1 border-stone-600 p-4">
+      <div className="">
+      {/* <img src="https://img.icons8.com/?size=100&id=24904&format=png&color=000000" className='w-5 h-5'></img> */}
+      <img src={TaskSvg} className='w-5 h-5'></img>
 
+        <div>Task 1</div>
+        {/* <svg xmlns="{TaskSvg}" width="1em" height="1em" fill="currentColor" viewBox="0 0 256 256"></svg> */}
+        <div>🗑</div>
+      </div>
+    </div>
+  );
+}
 function App() {
   return (
     <>
       <div className="bg-stone-950 text-white w-full h-full">
         <div className="flex bg-zinc-800 justify-between p-4">
-          <div className="w-1/2">
+          <div className="w-2/3">
             <img alt="Logo"></img>
           </div>
-          <div className="w-1/2 pr-4">
+          <div className="w-1/3 pr-4">
             <ul className="flex justify-between px-4">
-              <li>Home</li>
+              <li><div className='border-1 border-fuchsia-800 rounded-xl px-4 py-1'>Home</div></li>
               <li>About</li>
               <li>Services</li>
-              <li><button className='bg-fuchsia-800 rounded-xl px-4 py-1'>Contact</button></li>
+              <li><div className='bg-fuchsia-800 rounded-xl px-4 py-1'>Contact</div></li>
             </ul>
           </div>
         </div>
         <div>
-          <div className="w-full flex justify-center items-center px-10 py-20">
-            <h3 className="text-6xl font-bold">Simple Task Management App</h3>
+          <div className="w-full flex justify-center items-center px-10 pt-20">
+            <h3 className="text-8xl font-bold text-center">Simple Task <br></br>Management App</h3>
           </div>
           <div className="w-full flex flex-col justify-center py-10 px-80">
             <InputArea />
-            <div className="border-4 rounded-lg border-fuchsia-800 w-full mt-4 p-4 text-center">Task List</div>
+            {/* <button className="bg-fuchsia-800 text-white w-full rounded-lg p-4 mt-4">Add Task</button> */}
+          <TaskList />
+
           </div>
         </div>
       </div>
