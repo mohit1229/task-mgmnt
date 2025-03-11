e8f48d8e5e93f5a7f7a4ea0bd7d6ef549b2d2f9a
mohit1229 <kumarmohiiit123@gmail.com>
2025-03-12 02:33:13 +0530

    Fixing:CORS issue

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 const allowedOrigins = [
   "http://localhost:5173", // Development Frontend
   "https://your-frontend.vercel.app", // Production Frontend on Vercel
-  "https://task-mgmnt-alpha.vercel.app/",
+  "https://task-mgmnt-alpha.vercel.app",
 ];
 
 app.use(cors({
